5abf32dbb63c497a8332bdd575a362546e7d49bf
Cam1906 <123465742+Cam1906@users.noreply.github.com>
2024-03-05 10:15:32 +0000

    Update puzzle-selection.js

--- scripts/puzzle-selection.js
+++ scripts/puzzle-selection.js
@@ -92,8 +92,8 @@ const cardContainer = document.querySelector('.container')
 const functionCards = () => {
     arrayData.map((data) => {
         
-        cardContainer.innerHTML += '<div class="card" id="'+data.id+'">\
-                                        <div class="front" style="background-image: url(\'../assets/backgrounds/'+data.imgUrl+'\'); background-size: cover; background-position: 62.5%;">\
+        cardContainer.innerHTML += '<div class="card" id="'+data.id+'" style="background-image: url(\'../assets/backgrounds/'+data.imgUrl+'\');>\
+                                        <div class="front">\
                                             <h1>'+data.Heading+'</h1>\
                                             <button onclick="handleClick'+data.id+'()">Read More</button>\
                                         </div>\
