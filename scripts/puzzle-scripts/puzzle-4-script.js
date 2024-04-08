4b737084b4e9455b5e3e4db1763267affc4664e7
cl0ud <123465932+cl0udstar@users.noreply.github.com>
2024-04-08 22:52:55 +0100

    Update puzzle-4-script.js

--- scripts/puzzle-scripts/puzzle-4-script.js
+++ scripts/puzzle-scripts/puzzle-4-script.js
@@ -45,6 +45,7 @@ function checkSequence() {
         for (var j = 0; j < correctSet.length; j++) {
             if (checkedIds.includes(correctSet[j])) {
                 isChecked = true;
+                // console.log(checkedIds)
                 correctCheckedButtons.push(correctSet[j]); // Store ID of correct checked button
                 var container = document.getElementById('ww_' + correctSet[j]);
                 container.style.backgroundColor = 'green'; // Set background color to green for correct checked buttons
@@ -59,46 +60,79 @@ function checkSequence() {
     if (correct) {
         result.textContent = "Congratulations, Agent! You've found the best route through the checkpoints! The sail towards the village will be smooth now!";
         result.style.color = "green";
+        // alert("Congratulations! You've completed the sequence.");
     } else {
         result.textContent = "The route to the village will be full of winds, keep exploring, there is a better route!";
         result.style.color = "red";
+        // alert("Wrong Sequence!");
     }
     result.style.display = "block";
 
     // Display IDs of correct checked buttons (for testing)
+    // console.log("Correct checked buttons: ", correctCheckedButtons);
     wrongCheckedButtons = checkedIds.filter(value => !correctCheckedButtons.includes(value));
+    // console.log("Wrong checked buttons: ", wrongCheckedButtons);
 
     for (var i = 0; i < wrongCheckedButtons.length; i++) {
         var container = document.getElementById('ww_' + wrongCheckedButtons[i]);
-        container.style.backgroundColor = 'red';
+        container.style.backgroundColor = 'red'; // Set background color to green for correct checked buttons
     }
 }
 
+
+
+
 // First Question
-// var keyInputQ2 = document.getElementById("userAnswerQ2");
-// keyInputQ2.addEventListener("keypress", function(event) {
-//     if (event.key === "Enter") {
-//         event.preventDefault();
-//         document.getElementById("check-q2").click();
-//     }
-// });
+var keyInputQ1 = document.getElementById("userAnswerQ1");
+keyInputQ1.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("check-q1").click();
+    }
+});
 
-// function checkAnswerQ2() {
-//     var userAnswer = document.getElementById("userAnswerQ2").value.trim().toLowerCase();
-//     var result = document.getElementById("result2");
+function checkAnswerQ1() {
+    var userAnswer = document.getElementById("userAnswerQ1").value.trim().toLowerCase();
+    var result = document.getElementById("result1");
 
-//     if (userAnswer === "blue" || userAnswer === "wind speed 1" || userAnswer === "1") {
-//         result.textContent = "Nicely done Agent! Your spy game could outsmart even the fiercest Viking warriors.";
-//         result.style.color = "green";
-//     } else {
-//         result.textContent = "Looks like you've stumbled into a Viking hornet's nest, Agent. Try again!";
-//         result.style.color = "red";
-//     }
-//     result.style.display = "block";
-// }
+    if (userAnswer === "190" || userAnswer === "190 miles" || userAnswer === "190miles" || userAnswer === "190 miles per hour" || userAnswer === "190 mph" || userAnswer === "190mph") {
+        result.textContent = "Congratulations Agent, your stealth and cunning would make even Viking raiders bow in awe.";
+        result.style.color = "green";
+        keyInputQ1.disabled = true;
+    } else {
+        result.textContent = "Agent, I expect more from you. Keep exploring the data!";
+        result.style.color = "red";
+    }
+    result.style.display = "block";
+}
 
 
 // Second Question
+var keyInputQ2 = document.getElementById("userAnswerQ2");
+keyInputQ2.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("check-q2").click();
+    }
+});
+
+function checkAnswerQ2() {
+    var userAnswer = document.getElementById("userAnswerQ2").value.trim().toLowerCase();
+    var result = document.getElementById("result2");
+
+    if (userAnswer === "460" || userAnswer === "460 miles" || userAnswer === "460 miles per hour" || userAnswer === "460 mph" || userAnswer === "460mph" || userAnswer === "four hundred and sixty miles per hour" || userAnswer === "four hundred sixty") {
+        result.textContent = "Well done, Agent. Your finesse rivals that of the stealthiest Viking scouts.";
+        result.style.color = "green";
+        keyInputQ2.disabled = true;
+    } else {
+        result.textContent = "Agent, it seems you've hit a snag in your Viking escapade. Time to regroup and reassess.";
+        result.style.color = "red";
+    }
+    result.style.display = "block";
+}
+
+
+// Third Question
 var keyInputQ3 = document.getElementById("userAnswerQ3");
 keyInputQ3.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
@@ -111,35 +145,12 @@ function checkAnswerQ3() {
     var userAnswer = document.getElementById("userAnswerQ3").value.trim().toLowerCase();
     var result = document.getElementById("result3");
 
-    if (userAnswer === "190" || userAnswer === "190 miles" || userAnswer === "190 miles per hour" || userAnswer === "190 mph") {
-        result.textContent = "Congratulations Agent, your stealth and cunning would make even Viking raiders bow in awe.";
+    if (userAnswer === "blue" || userAnswer === "wind speed 1" || userAnswer === "1") {
+        result.textContent = "Nicely done Agent! Your spy game could outsmart even the fiercest Viking warriors.";
         result.style.color = "green";
+        keyInputQ3.disabled = true;
     } else {
-        result.textContent = "Agent, I expect more from you. Keep exploring the data!";
-        result.style.color = "red";
-    }
-    result.style.display = "block";
-}
-
-
-// Third Question
-var keyInputQ4 = document.getElementById("userAnswerQ4");
-keyInputQ4.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("check-q4").click();
-    }
-});
-
-function checkAnswerQ4() {
-    var userAnswer = document.getElementById("userAnswerQ4").value.trim().toLowerCase();
-    var result = document.getElementById("result4");
-
-    if (userAnswer === "460" || userAnswer === "460 miles" || userAnswer === "460 miles per hour" || userAnswer === "460 mph" || userAnswer === "460 mph") {
-        result.textContent = "Well done, Agent. Your finesse rivals that of the stealthiest Viking scouts.";
-        result.style.color = "green";
-    } else {
-        result.textContent = "Agent, it seems you've hit a snag in your Viking escapade. Time to regroup and reassess.";
+        result.textContent = "Looks like you've stumbled into a Viking hornet's nest, Agent. Try again!";
         result.style.color = "red";
     }
     result.style.display = "block";
