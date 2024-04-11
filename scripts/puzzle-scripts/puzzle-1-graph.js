f12b4de70a86cfcab32e8abe2ec79ad0596ae1f7
Cam1906 <123465742+Cam1906@users.noreply.github.com>
2024-04-11 14:08:54 +0100

    Update puzzle-1-graph.js

--- scripts/puzzle-scripts/puzzle-1-graph.js
+++ scripts/puzzle-scripts/puzzle-1-graph.js
@@ -219,6 +219,8 @@ function checkAnswerQ1() {
 
         result.textContent = "Agent, your skills are so sharp, even the Vikings would admire your craftiness.";
         result.style.color = "green";
+        let progress = require('../scripts/progress.json');
+        progress.puzzle1 = 'complete';
 
         var keyInputs = [keyInputQ1, keyInputQ2, keyInputQ3];
 
